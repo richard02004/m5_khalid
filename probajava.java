7a91c761a190720eb2965e8bcff3ea5138aac3e5
richard02004 <117456705+richard02004@users.noreply.github.com>
2022-12-20 12:12:33 +0100

    Update probajava.java

--- probajava.java
+++ probajava.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 public class probajava {
- 
     public static void main(String[] args) {
- 
         Scanner sc = new Scanner(System.in);
         String contraseña="eureka";
- 
         final int INTENTOS = 3;
-        
         //Esta variable booleana, nos controlara que en caso de que acierte la condicion cambie
         boolean acierto=false;
- 
         String password;
         for (int i=0;i<INTENTOS && !acierto;i++){
             System.out.println("Introduce una contraseña");
             password = sc.next();
- 
             //Comprobamos si coincide, no usamos ==, usamos el metodo equals
             if (password.equals(contraseña)){
                 System.out.println("Enhorabuena, acertaste");
